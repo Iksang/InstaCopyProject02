f8c5a8318a4474c7c847df0dab84a6b27c934be2
lee_se93 <lee_se93@naver.com>
2017-09-05 15:40:14 +0900

    BaseActivity

--- app/src/main/java/kr/co/tjeit/instacopyproject02/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject02/MainActivity.java
@@ -1,13 +1,31 @@
 package kr.co.tjeit.instacopyproject02;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseAcitivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+        setEvents();
+        setValues();
+    }
+
+    @Override
+    public void setEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
     }
 }
