ac572db9d74e115d5d1f653f12fe46862a306826
lee_se93 <lee_se93@naver.com>
2017-09-05 17:51:13 +0900

    LoginActivity XML, MainActivity XML

---
+++ app/src/main/java/kr/co/tjeit/instacopyproject02/LoginActivity.java
@@ -0,0 +1,31 @@
+package kr.co.tjeit.instacopyproject02;
+
+import android.os.Bundle;
+
+public class LoginActivity extends BaseAcitivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        bindViews();
+        setEvents();
+        setValues();
+    }
+
+    @Override
+    public void setEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
