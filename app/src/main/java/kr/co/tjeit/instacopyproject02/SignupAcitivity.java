0feea06c1b69ea92191f61f6db2a8c74dbc39479
lee_se93 <lee_se93@naver.com>
2017-09-07 11:42:06 +0900

    SignUpActivity

---
+++ app/src/main/java/kr/co/tjeit/instacopyproject02/SignupAcitivity.java
@@ -0,0 +1,45 @@
+package kr.co.tjeit.instacopyproject02;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignupAcitivity extends BaseAcitivity {
+
+    private android.widget.EditText idEdt;
+    private android.widget.Button checkDuplBtn;
+    private android.widget.EditText nameEdt;
+    private android.widget.EditText pwEdt;
+    private android.widget.EditText phoneEdt;
+    private android.widget.Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_acitivity);
+
+        bindViews();
+        setEvents();
+        setValues();
+    }
+
+    @Override
+    public void setEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        this.signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        this.phoneEdt = (EditText) findViewById(R.id.phoneEdt);
+        this.pwEdt = (EditText) findViewById(R.id.pwEdt);
+        this.nameEdt = (EditText) findViewById(R.id.nameEdt);
+        this.checkDuplBtn = (Button) findViewById(R.id.checkDuplBtn);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
+    }
+}
