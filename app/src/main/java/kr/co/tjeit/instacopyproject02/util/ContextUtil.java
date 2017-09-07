d6ccde2331286cf79c4556a4b35b8cea8dd2bf94
HanSangYeol <samh2006@naver.com>
2017-09-07 11:42:59 +0900

    20170907_AM11

---
+++ app/src/main/java/kr/co/tjeit/instacopyproject02/util/ContextUtil.java
@@ -0,0 +1,12 @@
+package kr.co.tjeit.instacopyproject02.util;
+
+/**
+ * Created by the on 2017-09-07.
+ */
+
+public class ContextUtil {
+
+    private static final String prefName = "InstagramPref";
+
+
+}
